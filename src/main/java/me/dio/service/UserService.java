67896a444380ddf000f1cb83d9189f1b1cfc705b
Williannca85 <williannca@hotmail.com>
2025-03-08 16:07:41 -0300

    Implementação da camada de negocios

---
+++ src/main/java/me/dio/service/UserService.java
@@ -0,0 +1,12 @@
+package me.dio.service;
+
+import me.dio.domain.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+
+   User findById(Long id);
+
+   User create(User userToCreate);
+}
---
+++ src/main/java/me/dio/service/impl/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package me.dio.service.impl;
+
+import me.dio.domain.model.User;
+import me.dio.domain.repository.UserRepository;
+import me.dio.service.UserService;
+import org.springframework.stereotype.Component;
+
+import java.util.NoSuchElementException;
+
+@Component
+public class UserServiceImpl implements UserService {
+
+
+   private final UserRepository userRepository;
+
+   public UserServiceImpl(UserRepository userRepository) {
+      this.userRepository = userRepository;
+   }
+
+
+   @Override
+   public User findById(Long id) {
+      return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+   }
+
+   @Override
+   public User create(User userToCreate) {
+      if(userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+         throw new IllegalArgumentException("User with account number " + userToCreate.getAccount().getNumber() + " already exists");
+      }
+      return userRepository.save(userToCreate);
+   }
+}
